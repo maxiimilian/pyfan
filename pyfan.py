7346337246a3d5d9b989ee664d2c9fef93558731
Maxiimilian <mail@maximilian-pierzyna.de>
2017-07-18 12:10:02 +0200

    Added destructor to set fan back to auto on exit. First strategy implemented

--- pyfan.py
+++ pyfan.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from time import sleep
 from fan import Fan
 from sensor import Sensor
@@ -8,6 +9,15 @@ class PyFan_Controller:
         self.__fans = []
 
         self._refresh = refresh
+
+    def __enter__(self):
+        """ return self to use PyFan_Controller with with statement """
+        return self
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        """ Set fans back to auto on exit """
+        for fan, st in self.__fans:
+            fan.set_auto()
 
     def add_sensor(self, sensor):
         self.__sensors.append(sensor)
@@ -24,28 +34,59 @@ class PyFan_Controller:
         # Start fan control loop
         while True:
             # Iterate over fans with associated strategy st
-            for f,st in self.__fans:
-                # Mean of mean.. a little ugly but only for testing
-                temp_30s = sum([s.temp_30s for s in self.__sensors])/len(self.__sensors)
-                if temp_30s>55:
-                    print("Mean temp_30s is {} -> fan auto".format(temp_30s))
-                    f.set_auto()
+            for fan, strategy in self.__fans:
+                strategy = strategy(fan, self.__sensors)
+                level = strategy.level
+                # Handle possible return values
+                if level in range(0,8):
+                    fan.set_level(level)
+                elif level == "auto":
+                    fan.set_auto()
+                elif level == "off":
+                    fan.set_off()
                 else:
-                    print("Mean temp_30s is {} -> fan off".format(temp_30s))
-                    f.set_off()
+                    # Fallback for security reasons
+                    fan.set_fullspeed()
+
+                #print(level)
 
             sleep(self._refresh)
 
 class PyFan_Strategy:
-    """ Implements a strategy to use to determine the fan speed level """
-    pass
+    """ Implements a strategy template to be used to determine the fan speed level """
+    def __init__(self, fan, sensors):
+        self._sensors = sensors
+        self._fan = fan
+
+    def get_fan_level(self):
+        """ Override this to implement your strategy """
+        pass
+
+    def has_changed(self):
+        """ @todo implemt something to determine if status chagned may level setter property ?"""
+        pass
+
+    @property
+    def level(self):
+        return self.get_fan_level()
+
 
 class Permissive_Strategy(PyFan_Strategy):
     """ 
-    temp_30s < 55 degree Celsius: Fan off 
+    temp_30s < 56 degree Celsius: Fan off 
     else: Fan auto
     """
-    pass
+    def get_fan_level(self):
+        # if any of the 30s average temps goes above 56 set fan to auto
+        for t in [s.temp_30s for s in self._sensors]:
+            if t > 56:
+                # exit loop and set fan to auto
+                self._level = "auto"
+                return self._level
+            else:
+                self._level = "off"
+
+        return self._level
 
 # Init fans
 fan1 = Fan("/proc/acpi/ibm/fan")
@@ -57,10 +98,10 @@ cpu3 = Sensor('/sys/devices/platform/coretemp.0/hwmon/hwmon2/temp3_input')
 ssd = Sensor('/sys/devices/virtual/hwmon/hwmon0/temp1_input')
 
 # Assign them to the controller
-controller = PyFan_Controller()
-controller.add_sensor(cpu1)
-controller.add_sensor(cpu2)
-controller.add_sensor(cpu3)
-controller.add_sensor(ssd)
-controller.add_fan(fan1, Permissive_Strategy)
-controller.start()
+with PyFan_Controller() as controller:
+    controller.add_sensor(cpu1)
+    controller.add_sensor(cpu2)
+    controller.add_sensor(cpu3)
+    controller.add_sensor(ssd)
+    controller.add_fan(fan1, Permissive_Strategy)
+    controller.start()
